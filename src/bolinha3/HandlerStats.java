2ef5b54709a1647d92b68e9a9a556db67a054dda
Daniel Quevedo <danielelvisquevedo@gmail.com>
2019-06-04 11:21:23 -0300

    HandlerStats added

---
+++ src/bolinha3/HandlerStats.java
@@ -0,0 +1,23 @@
+package bolinha3;
+
+import com.sun.management.OperatingSystemMXBean;
+import java.lang.management.ManagementFactory;
+
+public class HandlerStats {
+
+    public double cpu;
+    public double ram;
+    public double disk;
+    OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+
+    public HandlerStats() {
+    }
+
+    public double GetCPUUsage() {
+        return osBean.getProcessCpuLoad();
+    }
+
+    public double GetRAMUsage() {
+        return osBean.getFreePhysicalMemorySize();
+    }
+}
